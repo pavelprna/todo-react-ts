2d6ffae83d41074d70daa48759a36363a4045dd6
pavelprna <pavel@prna.ru>
2021-12-20 15:22:39 +0700

    add redux-toolkit

---
+++ src/store/todoSlice.ts
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+interface ITodo {
+  id: number
+  text: string
+  complited: boolean
+}
+
+interface ITodoState {
+  todos: ITodo[]
+}
+
+const todoSlice = createSlice({
+  name: 'todos',
+  initialState: {
+    todos: [],
+  } as ITodoState,
+  reducers: {
+    addTodo(state, action) {
+      state.todos.push({
+        id: Date.now(),
+        text: action.payload.text,
+        complited: false,
+      })
+    },
+    removeTodo(state, action) {},
+    toggleTodo(state, action) {},
+  },
+})
+
+export const { addTodo, removeTodo, toggleTodo } = todoSlice.actions
+
+export default todoSlice.reducer
